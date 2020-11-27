34535576446714aa73e9e08f25bcadabc66cae8a
alfredopc26 <calfredoperez26@gmail.com>
2020-11-26 21:59:17 -0500

    Revision final falta agregar categoria

--- api.py
+++ api.py
@@ -3,32 +3,44 @@ from app import app
 from con import mysql
 from flask import jsonify, request, flash, render_template
 
-
-#GET ALL
 @app.route('/')
 @app.route('/select', methods=['GET'])
 def lista():
     if request.method == 'GET':
         conn = mysql.connect()
         cur = conn.cursor(pymysql.cursors.DictCursor)
-        cur.execute("SELECT * FROM familia")
+        cur.execute("SELECT * FROM recursos.familia A LEFT JOIN recursos.categoria B ON A.categoria = B.idcategoria")
         rows = cur.fetchall()
     return render_template("lista.html", filas=rows)
 
 @app.route('/agregar_view', methods=['GET'])
 def agregar_view():
     if request.method == 'GET':
-        return render_template("agregar.html")
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        cur.execute("select * from recursos.categoria")
+        rows = cur.fetchall()
+        return render_template("agregar.html", categorias= rows)
 
 @app.route('/editar_view', methods=['GET'])
 def editar_view():
     identificacion = str(request.args.get('id'))
     conn = mysql.connect()
     cur = conn.cursor(pymysql.cursors.DictCursor)
-    cur.execute("select * from recursos.familia where identificacion='" + identificacion + "'")
-    rows = cur.fetchall()
-    return render_template("modificar.html", fila=rows)
+    cur.execute("SELECT * FROM recursos.familia A LEFT JOIN recursos.categoria B ON A.categoria = B.idcategoria and identificacion='" + identificacion + "'")
+    row = cur.fetchall()
+    cur.execute("select * from recursos.categoria")
+    rows_ = cur.fetchall()
+    return render_template("modificar.html", fila=row, categorias = rows_)
 
+@app.route('/lista_categoria', methods=['GET'])
+def lista_categoria():
+    if request.method == 'GET':
+        conn = mysql.connect()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        cur.execute("select * from recursos.categoria")
+        rows = cur.fetchall()
+        return render_template("lista_categoria.html", categorias= rows)
 
 #INSERT
 @app.route("/insertar", methods=["GET", "POST"])
@@ -40,10 +52,10 @@ def insertar_familia():
         apellido = request.form['apellido']
         edad = request.form['edad']
         celular = request.form['celular']
-        estado = request.form['estado']
+        categoria = request.form['categoria']
         conn = mysql.connect()
         cur = conn.cursor(pymysql.cursors.DictCursor)
-        query = "insert into familia (identificacion, nombre, apellido, edad, celular, Estado) values ('" + identificacion + "', '" + nombre + "', '" + apellido + "', '" + edad + "', '" + celular + "', '" + estado + "')"
+        query = "insert into familia (identificacion, nombre, apellido, edad, celular, categoria) values ('" + identificacion + "', '" + nombre + "', '" + apellido + "', '" + edad + "', '" + celular + "', '" + categoria + "')"
         cur.execute(query)
         conn.commit()
         cur.close()
@@ -61,10 +73,10 @@ def editar_familia():
         apellido = request.form['apellido']
         edad = request.form['edad']
         celular = request.form['celular']
-        estado = request.form['estado']
+        categoria = request.form['categoria']
         conn = mysql.connect()
         cur = conn.cursor(pymysql.cursors.DictCursor)
-        query = "update familia set nombre = '"+ nombre +"', apellido = '"+ apellido +"', edad = '"+ edad +"', celular = '"+ celular +"', estado = '"+ estado +"' where identificacion = '"+ identificacion +"'"
+        query = "update familia set nombre = '"+ nombre +"', apellido = '"+ apellido +"', edad = '"+ edad +"', celular = '"+ celular +"', categoria = '"+ categoria +"' where identificacion = '"+ identificacion +"'"
         cur.execute(query)
         conn.commit()
         cur.close()
@@ -72,7 +84,6 @@ def editar_familia():
     else:
         output = {'identificacion' : '', 'nombre' : '', 'Message': 'Error no type Post'}
     return jsonify({'result' : output})
-
 
 #DELETE
 @app.route('/borrar', methods=['POST'])
